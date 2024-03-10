e3e6350354985281f68cc9e137063f38d9aa84a4
백세종 <sejong418@icloud.com>
2024-03-10 21:03:23 +0900

    fix: 잘못된 import 경로 수정

--- backend/src/apps/file/domain/ports/event/publisher.py
+++ backend/src/apps/file/domain/ports/event/publisher.py
@@ -2,7 +2,7 @@ from typing import Protocol, runtime_checkable
 
 from spakky.domain.ports.event.event_publisher import IAsyncEventPublisher
 
-from apps.files.domain.models.file import File
+from apps.file.domain.models.file import File
 
 
 @runtime_checkable
